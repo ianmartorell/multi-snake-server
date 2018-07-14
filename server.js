e2ca996a0f2a13b5cc04491a79e7f22a49e20b8b
Ian Martorell <ianmartorell@gmail.com>
2018-07-14 22:26:15 +0200

    Hardcode port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import compression from 'compression';
 const app = express();
 const server = http.Server(app);
 const io = new SocketIO(server);
-const port = process.env.PORT || 3000;
+const port = 3000;
 let players = [];
 const sockets = {};
 
